57a2c1e3708bcb43a0a4e6d4f7cfa232a2cdfa8a
m0onsoo <kd.mpark10@gmail.com>
2026-09-15 17:05:27 -0700

    138 v2

--- problems/2026/week-01/138_moon.py
+++ problems/2026/week-01/138_moon.py
@@ -14,26 +14,37 @@ class Solution:
         hashmap = {} # key = original node, val = copied node
 
         node = head
-        while node:
-            # construct hashmap
-            node_cpy = Node(node.val)
-            hashmap[node] = node_cpy
-
-            node = node.next
-        
-        node = head
         root = prev = Node(0)
         while node:
+            if node not in hashmap:
+                # create corresponding node
+                node_cpy = Node(node.val)
+                hashmap[node] = node_cpy
+
             # connect next
             curr = hashmap[node]
             prev.next = curr
             prev = curr
 
             # connect random
-            rand = hashmap[node.random] if node.random else None
-            curr.random = rand
+            if node.random and hashmap.get(node.random):
+                # None이 아니고 랜덤노드 해시맵 연결도 되어있으면
+                rand = hashmap[node.random]
 
+                curr.random = rand
+            elif node.random:
+                # 랜덤 노드 존재하는 경우 새로만들어주기
+                rand = Node(node.random.val)
+                hashmap[node.random] = rand
+
+                curr.random = rand
+            else:
+                # 원본 노드 랜덤이 None이면
+                curr.random = None
+
+            # iterate
             node = node.next
+
         
         return root.next
 
